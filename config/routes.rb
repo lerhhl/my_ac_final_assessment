e09645cae48015533fbb63e43e3e0ffb7cdf66ef
lerhhl <lerhhl@gmail.com>
2017-07-06 11:17:00 +0800

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  
   devise_for :users
+  
+  root 'notes#index'
+
   resources :relationships, only: [:create, :destroy]
   resources :likes, only: [:create, :destroy]
   resources :notes
