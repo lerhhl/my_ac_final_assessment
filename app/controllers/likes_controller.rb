b2e75f8c3592a5a05c5b977b84feac9377a097bf
lerhhl <lerhhl@gmail.com>
2017-07-06 13:51:31 +0800

    Feature mailer

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -6,13 +6,16 @@ class LikesController < ApplicationController
     @user = current_user
     @like = current_user.likes.build(note_id: params[:note_id])
     @like.save
+    LikeMailer.liking_mail(@user, @note).deliver_now
     redirect_to note_path(@note)
   end
 
   def destroy
     @like = Like.find(params[:id])
     @note = Note.find(params[:note_id])
+    @user = current_user
     @like.destroy
+    LikeMailer.unliking_mail(@user, @note).deliver_now    
     redirect_to note_path(@note)
   end
 end
--- app/mailers/like_mailer.rb
+++ app/mailers/like_mailer.rb
@@ -1,2 +1,15 @@
 class LikeMailer < ApplicationMailer
+
+  def liking_mail(user, note)
+    @like_user = user
+    @note = note
+    mail to: @note.user.email, subject: "#{@like_user.email} has liked your #{@note.id}"
+  end
+  
+  def unliking_mail(user, note)
+    @unlike_user = user
+    @note = note
+    mail to: @note.user.email, subject: "#{@unlike_user.email} has unliked your #{@note.id}"  
+  end
+  
 end
